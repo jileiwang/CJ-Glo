30074a55f1a7608698cc43757cc0b2a1888ca5dc
jileiwang <wangjileiRUC@gmail.com>
2017-07-04 00:47:47 +0800

    on going

--- test/hash_mapping_table.h
+++ test/hash_mapping_table.h
@@ -20,21 +20,18 @@
 //  For more information, bug reports, fixes, contact:
 //    Jilei Wang (wangjileiRUC@gmail.com)
 
+#define SC2K_FILENAME "data/simplec2kanji.txt"
+#define K2SC_FILENAME "data/kanji2simplec.txt"
+
+#define SC2K_SIZE 5006
+#define K2SC_SIZE 5787
 
 typedef struct mapping_table_record {
   char original[4];
   // at most 7 corresponding characters
   char corresponding[22];
+  struct mapping_table_record *next;
 } MAPTABREC;
-
-MAPTABREC **k2sc, **sc2k;
-
-char *sc2k_filename = "../data/simplec2kanji.txt";
-char *k2sc_filename = "../data/kanji2simplec.txt";
-
-int sc2k_size = 5006;
-int k2sc_size = 5787;
-
 
 
 /**
